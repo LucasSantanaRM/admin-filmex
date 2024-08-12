a823d91269062e05bb3db12b166f388469ce67a9
LucasSantanaRM <83544692+LucasSantanaRM@users.noreply.github.com>
2024-08-12 12:30:26 -0400

    Ajuste do botão ver mais e inclusão do loading

--- admin-filmex/src/app/MoviesList/index.tsx
+++ admin-filmex/src/app/MoviesList/index.tsx
@@ -4,18 +4,19 @@ import "./index.scss";
 import axios from 'axios';
 import MovieCard from '../components/MovieCard';
 import { Movie } from '@/types/movie';
-
+import ReactLoading from 'react-loading';
+import { DiLaravel } from 'react-icons/di';
 
 
 export default function MovieList() {
     const [movies, setMovies] = useState<Movie[]>([]);
-
+    const [isLoading,setIsLoading] = useState<boolean>(true);
     useEffect(() => {
         getMovies();
     }, []);
 
-    const getMovies = () => {
-        axios({
+    const getMovies = async () => {
+       await axios({
             method: 'get',
             url: 'https://api.themoviedb.org/3/discover/movie',
             params: {
@@ -28,7 +29,20 @@ export default function MovieList() {
         }).catch(error => {
             console.error('Error fetching movies:', error);
         });
+
+        setIsLoading(false)
+
     };
+
+if (isLoading) {
+    return (
+        <div className='loading-container'>
+            <ReactLoading type="spin" color="#6046ff" height={'5%'} width={'5%'} />
+        </div>
+    )
+}
+
+
 
     return (
         <ul className="movie-list">
