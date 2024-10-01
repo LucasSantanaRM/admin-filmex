1170d684be7ad5891d43d756771022bfe54b511b
LucasSantanaRM <83544692+LucasSantanaRM@users.noreply.github.com>
2024-10-01 16:33:53 -0400

    add .env

--- admin-filmex/src/app/MoviesList/index.tsx
+++ admin-filmex/src/app/MoviesList/index.tsx
@@ -5,53 +5,65 @@ import axios from 'axios';
 import MovieCard from '../components/MovieCard';
 import { Movie } from '@/types/movie';
 import ReactLoading from 'react-loading';
-import { DiLaravel } from 'react-icons/di';
-
 
 export default function MovieList() {
     const [movies, setMovies] = useState<Movie[]>([]);
-    const [isLoading,setIsLoading] = useState<boolean>(true);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isLoadingMore, setIsLoadingMore] = useState<boolean>(false);
+    const [page, setPage] = useState<number>(1);
+
     useEffect(() => {
-        getMovies();
-    }, []);
+        getMovies(page);
+    }, [page]);
 
-    const getMovies = async () => {
-       await axios({
-            method: 'get',
-            url: 'https://api.themoviedb.org/3/discover/movie',
-            params: {
-                api_key: '3aae4e59ee042d41895da5b9ebf3b3a1',
-                language: 'pt-BR'
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - 200 && !isLoadingMore) {
+                setIsLoadingMore(true);
+                setPage(prevPage => prevPage + 1);
             }
-        }).then(response => {
-            console.log(response);
-            setMovies(response.data.results);
-        }).catch(error => {
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isLoadingMore]);
+
+    const getMovies = async (page: number) => {
+        try {
+            const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+                params: {
+                    api_key: process.env.NEXT_PUBLIC_API_KEY,
+                    language: 'pt-BR',
+                    page: page,
+                }
+            });
+
+            setMovies(prevMovies => [...prevMovies, ...response.data.results]);
+            setIsLoading(false);
+            setIsLoadingMore(false);
+        } catch (error) {
             console.error('Error fetching movies:', error);
-        });
-
-        setIsLoading(false)
-
+        }
     };
 
-if (isLoading) {
-    return (
-        <div className='loading-container'>
-            <ReactLoading type="spin" color="#6046ff" height={'5%'} width={'5%'} />
-        </div>
-    )
-}
-
-
+    if (isLoading) {
+        return (
+            <div className='loading-container'>
+                <ReactLoading type="spin" color="#6046ff" height={'5%'} width={'5%'} />
+            </div>
+        );
+    }
 
     return (
-        <ul className="movie-list">
+        <div className='movie-list'>
             {movies.map((movie) => (
-                <MovieCard
-                key={movie.id}
-                  movie={movie}
-                />
+                <MovieCard key={movie.id} movie={movie} />
             ))}
-        </ul>
+            {isLoadingMore && (
+                <div className='loading-container'>
+                    <ReactLoading type="spin" color="#6046ff" height={'5%'} width={'5%'} />
+                </div>
+            )}
+        </div>
     );
 }
